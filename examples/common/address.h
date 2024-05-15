ec97e2a533d3c4690e982b233dbb0b2248ca2990
shixinh <shixinh@localhost>
2024-05-15 05:48:30 -0500

    fix gen traffic

--- examples/common/address.h
+++ examples/common/address.h
@@ -2,18 +2,18 @@
 #define ADDRESS_H
 
 
-#define CLIENT_MAC "02:42:ac:12:00:03"
-#define SERVER_MAC "02:42:ac:13:00:03"
-#define ATTACKER_MAC "00:00:00:00:03"
-#define CLIENT_R_MAC "02:42:ac:12:00:02"
-#define SERVER_R_MAC "02:42:ac:13:00:02"
-#define ATTACKER_R_MAC "00:00:00:00:13"
-#define CLIENT_IP ("172.18.0.3")
-#define SERVER_IP ("172.19.0.3")
-#define ATTACKER_IP ("10.20.0.3")
+#define CLIENT_MAC "3c:fd:fe:b5:00:44"
+#define SERVER_MAC "3c:fd:fe:b4:fb:dc"
+#define ATTACKER_MAC "3c:fd:fe:b4:fc:c4"
+#define CLIENT_R_MAC "90:e2:ba:b3:21:71"
+#define SERVER_R_MAC "90:e2:ba:b3:21:70"
+#define ATTACKER_R_MAC "90:e2:ba:b3:21:71"
+#define CLIENT_IP ("10.18.0.3")
+#define SERVER_IP ("10.19.0.3")
+#define ATTACKER_IP ("10.18.0.4")
 #define CLIENT_R_IF_ORDER 0
 #define SERVER_R_IF_ORDER 1
-#define ATTACKER_R_IF_ORDER 1
+#define ATTACKER_R_IF_ORDER 0
 
 
 
